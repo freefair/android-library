3c34ce8d15bd94a9450f06eab789f2b1032b89cd
Lars Grefer <larsgrefer@freenet.de>
2014-11-25 13:31:40 +0100

    + @Inherited annotation for annotations

--- src/main/java/de/larsgrefer/android/library/injection/annotation/XmlLayout.java
+++ src/main/java/de/larsgrefer/android/library/injection/annotation/XmlLayout.java
@@ -3,6 +3,7 @@ package de.larsgrefer.android.library.injection.annotation;
 import android.support.annotation.LayoutRes;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -12,6 +13,7 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface XmlLayout {
 	@LayoutRes
 	public int id();
--- src/main/java/de/larsgrefer/android/library/injection/annotation/XmlView.java
+++ src/main/java/de/larsgrefer/android/library/injection/annotation/XmlView.java
@@ -3,6 +3,7 @@ package de.larsgrefer.android.library.injection.annotation;
 import android.support.annotation.IdRes;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -12,6 +13,7 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface XmlView {
 	@IdRes
 	public int id() default DEFAULT_ID;
